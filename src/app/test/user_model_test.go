e35028e1896ce9d3e263196b257a1ab542f20eaf
Kobayashi(attela) <kobayashi@attela.jp>
2017-07-09 03:02:52 +0900

    add assert

--- src/app/test/user_model_test.go
+++ src/app/test/user_model_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"app/models"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_ユーザー登録成功(t *testing.T) {
+	assert := assert.New(t)
 	save := models.User{
 		Name:     "hoge",
-		Email:    models.NewNullString("hogeahogecom"),
+		Email:    models.NewNullString("hoge@hogecom"),
 		Password: models.NewNullString("hogehoge"),
 	}
 	data, err := models.CreateUser(save)
@@ -16,7 +18,19 @@ func Test_ユーザー登録成功(t *testing.T) {
 		t.Log(err)
 		t.Fatalf("err")
 	}
-	if data.Token != "" {
-		t.Log(data)
+	assert.NotEqual("", data.Token, "トークンが取得できている")
+}
+
+func Test_ユーザーログイン成功(t *testing.T) {
+	assert := assert.New(t)
+	save := models.User{
+		Email:    "hoge@hogecom",
+		Password: "hogehoge",
 	}
+	data, err := models.Login(save)
+	if err != nil {
+		t.Log(err)
+		t.Fatalf("err")
+	}
+	assert.NotEqual("", data.Token, "トークンが取得できている")
 }
